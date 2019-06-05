97c5a08ba6d3689203df734631bad78a9dc3da6d
Kelsey Low <kklowlow@gmail.com>
2019-06-05 02:23:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch 'articles/:id', to: 'articles#update'
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
   
   # resources :articles, only: [:index, :show, :new, :create, :edit, :update]
   # ^ will give the same routes, but with a PUT route for update rather than PATCH
