b720a67b2f85ad9feaa1ad3c9b693b08cb060791
Kelsey Low <kklowlow@gmail.com>
2019-06-05 02:24:14 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
   # patch 'articles/:id', to: 'articles#update'
-  
-  # resources :articles, only: [:index, :show, :new, :create, :edit, :update]
   # ^ will give the same routes, but with a PUT route for update rather than PATCH
+  
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  
   
 end
